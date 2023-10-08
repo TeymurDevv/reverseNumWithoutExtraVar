bb01706e4bf23e4ae0aaed053f2181bd407c4ee0
TeymurDevv <transmissiyaservis@gmail.com>
2023-10-08 11:37:03 +0400

    Created a simple c# Code that reversees numbers without extra variables

---
+++ reverseNumWithoutExtraVar/reverseNumWithoutExtraVar/Program.cs
@@ -0,0 +1,7 @@
+﻿int num1 = 49;
+int num2 = 44;
+num1 = num1 + num2; // num1= 49+44=93
+num2 = num1 - num2; // num2= 93-44=49
+num1 = num1 - num2; // num1=93-49=44
+Console.WriteLine(num1);
+Console.WriteLine(num2);
